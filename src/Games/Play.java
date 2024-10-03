9e9d067301e3bba89822511ab534b107dc519743
Zahaawii <141314605+Zahaawii@users.noreply.github.com>
2024-10-03 10:28:21 +0200

    Updated comments.java

--- src/Games/Play.java
+++ src/Games/Play.java
@@ -5,11 +5,14 @@ import UI.ConsoleOperatos;
 
 import java.util.Random;
 
+//Creating a class so the user has the opportunity to play with their pet, If the pet is low on energy the pet will go to sleep instead.
 public class Play {
 
+     //Initialize the Tamagotchi object to the class
     static TamagotchiCharacters c1 = new TamagotchiCharacters(TamagotchiCharacters.getName(), TamagotchiCharacters.getRace(), TamagotchiCharacters.getAge(), TamagotchiCharacters.getMood(), TamagotchiCharacters.getEnergy());
     static Random rd = new Random();
 
+    //Creating a method for the pet to be played with
     public static void playWithPet() {
 
         int randomEnergy = rd.nextInt(99);
@@ -20,7 +23,9 @@ public class Play {
             System.out.println(TamagotchiCharacters.getName() + " Is really ready to play");
             ConsoleOperatos.printSeperator(30);
 
-        } else if(TamagotchiCharacters.getEnergy() < 100 && TamagotchiCharacters.getEnergy() > 50) {
+        } 
+        
+        else if(TamagotchiCharacters.getEnergy() < 100 && TamagotchiCharacters.getEnergy() > 50) {
             System.out.println(TamagotchiCharacters.getName() + " energy is: " + TamagotchiCharacters.getEnergy());
             ConsoleOperatos.printSeperator(30);
             System.out.println(TamagotchiCharacters.getName() + " is a little tired but ready to play");
