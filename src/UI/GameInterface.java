9e33f52b91614fa414d2e29cb89dbe43d0f060f2
Zahaawii <141314605+Zahaawii@users.noreply.github.com>
2024-10-03 10:38:24 +0200

    Update GameInterface.java

--- src/UI/GameInterface.java
+++ src/UI/GameInterface.java
@@ -1,4 +1,5 @@
 package UI;
+//Importing all the packages from the game
 import Games.Eat;
 import Games.Play;
 import Games.SeeAttributes;
@@ -6,10 +7,12 @@ import Games.Sleep;
 import Pets.TamagotchiCharacters;
 import java.util.Scanner;
 
+//Create a UI interface for the users game so it looks nicely.
 public class GameInterface {
 
     static Scanner scanner = new Scanner(System.in);
 
+    //Creating a method to showcase the welcome menu
     public static void welcomeGameInterface() {
         ConsoleOperatos.displayText("Welcome to your Tamagotchi world: " + TamagotchiCharacters.getName());
         System.out.println("You can see your pet, feed, play or let: " + TamagotchiCharacters.getName() + " sleep");
@@ -17,6 +20,7 @@ public class GameInterface {
                 "\nPress 3 Play \nPress 4 Sleep");
     }
 
+    //let the user decide what to do. If wrong input received it will loop until the user input correct
     public static void userDecisionGame() {
         int userDecision;
 
@@ -29,6 +33,7 @@ public class GameInterface {
             }
         } while (userDecision <= 0 || userDecision > 4);
 
+        //The user has 4 options for their pet.
         switch(userDecision) {
             case 1:
                 SeeAttributes.seePetAttributes();
