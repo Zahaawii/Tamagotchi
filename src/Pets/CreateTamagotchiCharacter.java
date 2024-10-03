4d738bf20248878e3e51afeafd72a81ee747e0f3
Zahaawii <141314605+Zahaawii@users.noreply.github.com>
2024-10-03 10:34:27 +0200

    Updating comments.java

--- src/Pets/CreateTamagotchiCharacter.java
+++ src/Pets/CreateTamagotchiCharacter.java
@@ -2,16 +2,21 @@ package Pets;
 import java.util.List;
 import java.util.ArrayList;
 
+//Created a class to take the attributes and place them in an Arraylist.
+
 public class CreateTamagotchiCharacter {
 
+    //Initializing the arrayList
     static private List<TamagotchiCharacters> characters = new ArrayList<>();
 
+    //Create an Arraylist method to take the Attributes and store them in the Array.
     public TamagotchiCharacters makeCharacter(String name, String race, int age, String mood, int energy) {
         TamagotchiCharacters character = new TamagotchiCharacters(name, race, age, mood, energy);
         characters.add(character);
         return character;
     }
 
+    //Create a method to showcase the characters
     public List<TamagotchiCharacters> getCharacters() {
         return characters;
     }
