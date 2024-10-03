d2d733c7e97192eaefb5de4733e3d2d7e3e9c44f
Zahaawii <141314605+Zahaawii@users.noreply.github.com>
2024-10-03 10:29:25 +0200

    Upda

--- src/Games/SeeAttributes.java
+++ src/Games/SeeAttributes.java
@@ -3,11 +3,15 @@ package Games;
 import Pets.CreateTamagotchiCharacter;
 import Pets.TamagotchiCharacters;
 
+//Created a class for the user to see his pets attributes, name,race, ace, mood and energy
 public class SeeAttributes {
+
+        //Initialize the Tamagotchi object to the class
 
     static TamagotchiCharacters c1 = new TamagotchiCharacters(TamagotchiCharacters.getName(), TamagotchiCharacters.getRace(), TamagotchiCharacters.getAge(), TamagotchiCharacters.getMood(), TamagotchiCharacters.getEnergy());
 
 
+    //Made a for-each loop to showcase all attributes if the name is not null. See the foreach under Pets.
     public static void seePetAttributes() {
         CreateTamagotchiCharacter.getTamagotchiCharacters(TamagotchiCharacters.getName());
     }
